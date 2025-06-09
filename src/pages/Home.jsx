5871a8727c6c37874b5ac47022439c51630553ee
Deepanshu Choudhary <137767352+Deepanshu-choudhary1@users.noreply.github.com>
2025-06-09 18:47:13 +0530

    corrected getpost

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ export default function Home() {
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const res = await fetch(`${BACKEND_URL}/api/post/getPosts`);
+      const res = await fetch(`${BACKEND_URL}/api/post/getposts`);
       const data = await res.json();
       setPosts(data.posts);
     };
