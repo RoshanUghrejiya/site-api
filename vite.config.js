ac16a70f55ce2961420c90c964b8a84348a98f76
Deepanshu Choudhary <137767352+Deepanshu-choudhary1@users.noreply.github.com>
2025-06-12 18:34:06 +0530

    corrected backend path

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   build: {
     outDir: 'dist',
+    emptyOutDir: true,
   },
   server: {
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
+        changeOrigin: true,
         secure: false,
       },
     },
-    historyApiFallback: true,
   },
-  plugins: [react()],
 });
-
-
